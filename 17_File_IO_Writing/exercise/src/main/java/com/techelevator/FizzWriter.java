6937bae74571e8088fe39f2710c7c769041d767c
Kory Allison <koryrallison@gmail.com>
2022-04-03 17:56:38 +0000

    Update FizzWriter.java

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,8 +1,40 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+
+		String fileName;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the destination file:");
+		fileName = sc.nextLine();
+		sc.close();
+
+		try {
+			PrintWriter w = new PrintWriter(fileName);
+
+			for(int n = 1; n <= 300; n++){
+				if(n % 3 == 0 && n % 5 == 0){
+					w.println("FizzBuzz");
+				}
+				else if (n % 3 == 0){
+					w.println("Fizz");
+				}
+				else if (n % 5 == 0){
+					w.println("Buzz");
+				}
+				else
+					w.println(n);
+			}
+			w.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: Invalid destination file.");
+		}
+
 
 	}
 
